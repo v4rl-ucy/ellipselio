2c99395351a5ed7f3fe0e55e6bde6beb8c290960
Rowan Border <rowanjborder@gmail.com>
2024-06-12 16:56:37 +0300

    implementing cam projection class

--- include/cam_processing.h
+++ include/cam_processing.h
@@ -3,23 +3,22 @@
 #ifndef CAM_PROCESSING_H
 #define CAM_PROCESSING_H
 
-#include <math.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <ros/ros.h>
-#include <sensor_msgs/CameraInfo.h>
-#include <sensor_msgs/PointCloud2.h>
-
-#include <Eigen/Eigen>
+#include "cv_bridge/cv_bridge.h"
+#include "image_transport/image_transport.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "rclcpp/logging.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 class CamProcess {
  public:
   CamProcess();
-  ~CamProcess();
 
  private:
-  ros::Subscriber sub_cam;
-  Eigen::Isometry3d T_cam_lidar;
-  Eigen::Vector
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber cam_sub_;
+  Eigen::Isometry3d T_cam_lidar_;
+
+  void CamCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg);
 };
 
 #endif  // SEE_CAM_PROCESSING_H
--- src/cam_processing.cpp
+++ src/cam_processing.cpp
@@ -1,3 +1,5 @@
 #include <cam_processing.h>
 
-CamProcess::CamProcess() { ROS_INFO("CamProcess constructor"); }
+CamProcess::CamProcess(std::string cam_topic, ) : it_(node_) {
+  cam_sub_ = it_.subscribe(cam_topic, 1, &CamProcess::CamCallback, this);
+}
