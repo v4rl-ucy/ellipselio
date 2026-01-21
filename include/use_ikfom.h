0d32861bc1a29f17ce944a3f571887b9414b4bcc
Rowan Border <rowanjborder@gmail.com>
2026-01-21 21:17:19 +0200

    reset ikfom state

--- include/use_ikfom.h
+++ include/use_ikfom.h
@@ -3,6 +3,7 @@
 #ifndef USE_IKFOM_H
 #define USE_IKFOM_H
 
+#define BOOST_BIND_GLOBAL_PLACEHOLDERS
 #include <IKFoM_toolkit/esekfom/esekfom.hpp>
 
 typedef MTK::vect<3, double> vect3;
@@ -11,10 +12,9 @@ typedef MTK::S2<double, 98090, 10000, 1> S2;
 typedef MTK::vect<1, double> vect1;
 typedef MTK::vect<2, double> vect2;
 
-MTK_BUILD_MANIFOLD(state_ikfom,
-                   ((vect3, pos))((SO3, rot))((SO3, offset_R_L_I))(
-                       (vect3, offset_T_L_I))((vect3, linvel))((vect3, angvel))(
-                       (vect3, bg))((vect3, ba))((S2, grav)));
+MTK_BUILD_MANIFOLD(state_ikfom, ((vect3, pos))((SO3, rot))((SO3, offset_R_L_I))(
+                                    (vect3, offset_T_L_I))((vect3, vel))(
+                                    (vect3, bg))((vect3, ba))((S2, grav)));
 
 MTK_BUILD_MANIFOLD(input_ikfom, ((vect3, acc))((vect3, gyro)));
 
@@ -33,62 +33,51 @@ inline Ikfom::cov P_cov() {
 
 inline MTK::get_cov<process_noise_ikfom>::type process_noise_cov() {
   MTK::get_cov<process_noise_ikfom>::type cov =
-      MTK::get_cov<process_noise_ikfom>::type::Zero();
-  MTK::setDiagonal<process_noise_ikfom, vect3, 0>(cov, &process_noise_ikfom::ng,
-                                                  1e-3);
-  MTK::setDiagonal<process_noise_ikfom, vect3, 3>(cov, &process_noise_ikfom::na,
-                                                  1e-3);
-  MTK::setDiagonal<process_noise_ikfom, vect3, 6>(
-      cov, &process_noise_ikfom::nbg, 1e-3);
-  MTK::setDiagonal<process_noise_ikfom, vect3, 9>(
-      cov, &process_noise_ikfom::nba, 1e-3);
+      MTK::get_cov<process_noise_ikfom>::type::Identity();
+  cov *= 1e-3;
   return cov;
 }
 
-inline Eigen::Matrix<double, 27, 1> get_f(state_ikfom& s,
+inline Eigen::Matrix<double, 24, 1> get_f(state_ikfom& s,
                                           const input_ikfom& in) {
-  Eigen::Matrix<double, 27, 1> res = Eigen::Matrix<double, 27, 1>::Zero();
+  Eigen::Matrix<double, 24, 1> res = Eigen::Matrix<double, 24, 1>::Zero();
   vect3 omega;
   in.gyro.boxminus(omega, s.bg);
   vect3 a_inertial = s.rot * (in.acc - s.ba);
   for (int i = 0; i < 3; i++) {
-    res(i) = s.linvel[i];
+    res(i) = s.vel[i];
     res(i + 3) = omega[i];
     res(i + 12) = a_inertial[i] + s.grav[i];
-    res(i + 15) = omega[i];
   }
   return res;
 }
 
-inline Eigen::Matrix<double, 27, 26> df_dx(state_ikfom& s,
+inline Eigen::Matrix<double, 24, 23> df_dx(state_ikfom& s,
                                            const input_ikfom& in) {
-  Eigen::Matrix<double, 27, 26> cov = Eigen::Matrix<double, 27, 26>::Zero();
+  Eigen::Matrix<double, 24, 23> cov = Eigen::Matrix<double, 24, 23>::Zero();
   cov.template block<3, 3>(0, 12) = Eigen::Matrix3d::Identity();
   vect3 acc_;
   in.acc.boxminus(acc_, s.ba);
   vect3 omega;
   in.gyro.boxminus(omega, s.bg);
   cov.template block<3, 3>(12, 3) = -s.rot.toRotationMatrix() * MTK::hat(acc_);
-  cov.template block<3, 3>(12, 21) = -s.rot.toRotationMatrix();
+  cov.template block<3, 3>(12, 18) = -s.rot.toRotationMatrix();
   Eigen::Matrix<state_ikfom::scalar, 2, 1> vec =
       Eigen::Matrix<state_ikfom::scalar, 2, 1>::Zero();
   Eigen::Matrix<state_ikfom::scalar, 3, 2> grav_matrix;
-  s.S2_Mx(grav_matrix, vec, 24);
-  cov.template block<3, 2>(12, 24) = grav_matrix;
-  cov.template block<3, 3>(3, 18) = -Eigen::Matrix3d::Identity();
-  cov.template block<3, 3>(3, 15) = Eigen::Matrix3d::Identity();
-  cov.template block<3, 3>(15, 18) = -Eigen::Matrix3d::Identity();
+  s.S2_Mx(grav_matrix, vec, 21);
+  cov.template block<3, 2>(12, 21) = grav_matrix;
+  cov.template block<3, 3>(3, 15) = -Eigen::Matrix3d::Identity();
   return cov;
 }
 
-inline Eigen::Matrix<double, 27, 12> df_dw(state_ikfom& s,
+inline Eigen::Matrix<double, 24, 12> df_dw(state_ikfom& s,
                                            const input_ikfom& in) {
-  Eigen::Matrix<double, 27, 12> cov = Eigen::Matrix<double, 27, 12>::Zero();
+  Eigen::Matrix<double, 24, 12> cov = Eigen::Matrix<double, 24, 12>::Zero();
   cov.template block<3, 3>(12, 3) = -s.rot.toRotationMatrix();
   cov.template block<3, 3>(3, 0) = -Eigen::Matrix3d::Identity();
-  cov.template block<3, 3>(18, 6) = Eigen::Matrix3d::Identity();
-  cov.template block<3, 3>(21, 9) = Eigen::Matrix3d::Identity();
-  cov.template block<3, 3>(15, 0) = -Eigen::Matrix3d::Identity();
+  cov.template block<3, 3>(15, 6) = Eigen::Matrix3d::Identity();
+  cov.template block<3, 3>(18, 9) = Eigen::Matrix3d::Identity();
   return cov;
 }
 
